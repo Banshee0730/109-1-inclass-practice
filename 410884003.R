e8338917510a202f42844243f9346490b3d60f5e
Banshee0730 <71749850+Banshee0730@users.noreply.github.com>
2020-09-30 15:05:31 +0800

    Add files via upload

---
+++ 410884003.R
@@ -0,0 +1,21 @@
+3 -> b
+a = 2
+my_108_total_credits <- 15
+2
+my_108_total_credits <- 15
+_108_total_credits <- 15
+108_total_credits <- 15
+_my_108_total_credits <- 15
+my.108.total_credits <- 15
+.108.total_credits <- 15 # 隱藏變數
+.my.108.total_credits <- 15
+我的108學年總學分數 <- 15
+`我的108學年總學分數` <- 15 # 特殊名稱處理，`不是名稱的一部份
+`.108.total_credits` <- 15
+
+a <- 3
+b
+sum(a+b)
+num1 <- 5
+vNum1 <- c(2,5)
+vNum2 <- c(2, num1)
